079678d148620af798dd2bae85d25d6b7887c4b0
Nikhil <nikhilashodariya123@gmail.com>
2019-11-13 20:07:35 -0500

    Adding changes

--- data/task.js
+++ data/task.js
@@ -4,10 +4,10 @@ const mongoCollections = require("../config/mongoCollections");
 
 const task = mongoCollections.task;
 
-getAllTask = async (skip, take) => {
+getAllTask = async () => {
     taskCollection = await task();
     return taskCollection.find({}).skip(skip).limit(take).toArray();
-}
+};
 
 getTaskById = async (id) => {
     // we need to search for that id;
@@ -22,7 +22,7 @@ getTaskById = async (id) => {
     }
 
     return data;
-}
+};
 
 postTask = async (userData) => {
     taskCollection = await task();
@@ -40,124 +40,11 @@ postTask = async (userData) => {
     return ops[0];
 
     // return the creadted object
-}
+};
 
-putTask = async (suppliedId, newData) => {
-    taskCollection = await task();
-    const data = await getTaskById(suppliedId);
-
-    if (data === null) {
-        return undefined;
-    } else {
-        const newValues = {
-            "$set": {
-                "title": newData.title,
-                "description": newData.description,
-                "hoursEstimated": newData.hoursEstimated,
-                "completed": newData.completed,
-            }
-        };
-        const taskCollection = await task();
-        await taskCollection.updateOne({
-            _id: ObjectId(suppliedId)
-        }, newValues);
-        const toReturn = await getTaskById(suppliedId);
-        return toReturn;
-    }
-}
-
-patchTask = async (suppliedId, newData) => {
-    taskCollection = await task();
-    const data = await getTaskById(suppliedId);
-
-    if (data === null) {
-        return undefined;
-    } else {
-        const newValues = {
-            "$set": {
-                "title": newData.title ? newData.title : data.title,
-                "description": newData.description ? newData.description : data.description,
-                "hoursEstimated": newData.hoursEstimated ? newData.hoursEstimated : data.hoursEstimated,
-                "completed": newData.completed ? newData.completed : data.completed,
-            }
-        };
-        const taskCollection = await task();
-        await taskCollection.updateOne({
-            _id: ObjectId(suppliedId)
-        }, newValues);
-        return await getTaskById(suppliedId);
-    }
-}
-
-postTaskComments = async (suppliedId, newComment) => {
-    taskCollection = await task();
-    const data = await getTaskById(suppliedId);
-
-    if (data === null) {
-        return undefined;
-    } else {
-        const value = {
-            "_id": new ObjectId(),
-            "name": newComment.name,
-            "comment": newComment.comment
-        }
-        data["comments"].push(value);
-        const newValues = {
-            "$set": {
-                ...data
-            }
-        };
-
-        await taskCollection.updateOne({
-            _id: ObjectId(suppliedId)
-        }, newValues);
-        return await getTaskById(suppliedId);
-    }
-}
-
-deleteTaskComments = async (taskId, commentId) => {
-    taskCollection = await task();
-    const data = await getTaskById(taskId);
-
-    if (data === null) {
-        return undefined;
-    } else {
-        [isPresent, newComments] = findCommentById(data["comments"], commentId);
-        if (isPresent) {
-            const newValues = {
-                "$set": {
-                    "comments": newComments
-                }
-            };
-            await taskCollection.updateOne({
-                _id: ObjectId(taskId)
-            }, newValues);
-            return await getTaskById(taskId);
-        } else {
-            return null;
-        }
-    }
-}
-
-findCommentById = (allData, commentId) => {
-    newData = []
-    isPresent = false;
-    for (val of allData) {
-        if (val["_id"] == commentId) {
-            isPresent = true;
-        } else {
-            newData.push(val);
-        }
-    }
-    return [isPresent, newData];
-}
 
 module.exports = {
     "getAllTask": getAllTask,
     "getTaskById": getTaskById,
-    "postTask": postTask,
-    "putTask": putTask,
-    "patchTask": patchTask,
-    "postTaskComments": postTaskComments,
-    "deleteTaskComments": deleteTaskComments
+    "postTask": postTask
 };
